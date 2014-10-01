c0b2f6f87cf0f9f508fcc76ba5b001e3df761a70
Chrono Monochrome <chrono.monochrome@gmail.com>
2014-10-01 22:04:58 +0400

    laba 1.5.1

---
+++ examples/laba1_5_1/Project1.dpr
@@ -0,0 +1,13 @@
+program Project1;
+
+uses
+  Forms,
+  Unit1_1 in '..\laba1_1_5\Unit1_1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
